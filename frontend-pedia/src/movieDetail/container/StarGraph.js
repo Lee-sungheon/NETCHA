5312626fa7d2e5b09956785f117278c350c0f7ff
강민창 <new0822@naver.com>
2021-03-25 22:04:35 +0900

    0325_FE_pedia : 출연자,별점,코멘트,갤러리 화면구성

---
+++ frontend-pedia/src/movieDetail/container/StarGraph.js
@@ -0,0 +1,13 @@
+import Graph from '../component/Graph';
+
+export default function StarGraph() {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  return (
+    <div className="starGraph">
+      <div className="infoHeader">
+        별점 그래프<div>평균 ★3.8</div>
+      </div>
+      <Graph rankArr={arr} />
+    </div>
+  );
+}
