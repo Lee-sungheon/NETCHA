a5acc71ccdea8ad1c5efa490646c35512023014b
강유정 <kos9811@naver.com>
2021-03-28 22:22:21 +0900

    0328_FE_pedia : 헤더 검색 기능 진행중

--- frontend-pedia/src/index.js
+++ frontend-pedia/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import store from './common/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root"),
 );
