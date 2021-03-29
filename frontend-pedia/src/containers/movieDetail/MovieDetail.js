73697dd24699460d8cdcc112f0bd9cf9e06bafcb
강민창 <new0822@naver.com>
2021-03-29 12:56:06 +0900

    0329_FE_pedia : 폴더구조 변경

--- frontend-pedia/src/containers/movieDetail/MovieDetail.js
+++ frontend-pedia/src/containers/movieDetail/MovieDetail.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './MovieDetail.scss';
-import BasicInfo from '../component/BasicInfo';
-import Actors from '../component/Actors';
-import StarGraph from '../component/StarGraph';
-import Comment from '../component/Comment/Comment';
-import Gallery from '../component/Gallery';
-import Video from '../component/Video';
-import SimilarMovies from '../component/SimilarMovies';
-import MovieHeader from '../component/MovieHeader';
+import BasicInfo from '../../movieDetail/component/BasicInfo';
+import Actors from '../../movieDetail/component/Actors';
+import StarGraph from '../../movieDetail/component/StarGraph';
+import Comment from '../../components/movieDetail/Comment/Comment';
+import Gallery from '../../movieDetail/component/Gallery';
+import Video from '../../movieDetail/component/Video';
+import SimilarMovies from '../../movieDetail/component/SimilarMovies';
+import MovieHeader from '../../movieDetail/component/MovieHeader';
 import { useDispatch, useSelector } from 'react-redux';
 
-export default function Home() {
+export default function MovieDetail() {
   const dispatch = useDispatch();
 
   return (
