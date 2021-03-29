040865d897555b896bb71127b62dabf564646f03
강민창 <new0822@naver.com>
2021-03-29 15:05:34 +0900

    0329_FE_pedia : 구조 변경
    Former-commit-id: 0b1003bb76232152b3f661dbdeede9ac55004f8a

--- frontend-pedia/src/App.js
+++ frontend-pedia/src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import User from "./pages/User";
 import UserStatics from "./pages/UserStatics";
-// import MovieDetail from './movieDetail/container/MovieDetail';
+import MovieDetail from './pages/MovieDetailPage';
 import "./App.scss";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Route exact path="/" component={MovieRanking} />
         <Route exact path="/user/:id" component={User} />
         <Route exact path="/user/statics/:id" component={UserStatics} />
-        {/* <Route exact path="/movieDetail/:movieId" component={MovieDetail} /> */}
+        <Route exact path="/movieDetail/:movieId" component={MovieDetail} />
       </BrowserRouter>
     </div>
   );
