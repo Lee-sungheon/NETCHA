6bba3ff17ec8b5bc06de1f9791cdad9fdd2702ae
강유정 <kos9811@naver.com>
2021-04-01 01:28:01 +0900

    0401_FE_pedia : 사용자 선호태그 데이터 연결 완료
    Former-commit-id: b056c4102dccd55a86ae0276168908286a2dac4c

--- frontend-pedia/src/components/user/WordCloud.js
+++ frontend-pedia/src/components/user/WordCloud.js
@@ -1,22 +1,26 @@
-import React from "react";
-import ReactWordcloud from "react-wordcloud";
-import "tippy.js/dist/tippy.css";
-import "tippy.js/animations/scale.css";
+import React from 'react';
+import ReactWordcloud from 'react-wordcloud';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale.css';
 
 const options = {
-  colors: ["rgb(255, 47, 110)"],
-  fontStyle: "Noto Sans KR",
+  colors: ['rgb(255, 47, 110)'],
+  fontStyle: 'Noto Sans KR',
   fontSizes: [10, 30],
-  fontWeight: "bold",
+  fontWeight: 'bold',
   padding: 10,
   rotationAngles: [0, 0],
   rotations: 0,
 };
 
-const Wordcloud = ({ words }) => {
+const Wordcloud = ({ tags, error, loading }) => {
+  if (error) {
+    return <h2>에러가 발생했습니다.</h2>;
+  }
+
   return (
-    <ReactWordcloud options={options} words={words}/>
+    <>{!loading && tags && <ReactWordcloud options={options} words={tags} />}</>
   );
-}
+};
 
 export default Wordcloud;
