0beaa1261ca26e2d87a48d288c444bb80541efff
강유정 <kos9811@naver.com>
2021-04-01 00:43:49 +0900

    0401_FE_pedia : 사용자 선호 감독, 선호 배우 데이터 연결
    Former-commit-id: 8f9d400fadfb2b37f8a54b1d737c441886976952

--- frontend-pedia/src/components/user/PersonImage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function PersonImage({ name }) {
-  console.log({name}.name);
-  const imageSrc = "/images/" + { name }.name + ".jpg";
-  
-  return <img className="actorImage" src={imageSrc} alt={name} title={name} />;
-}
--- frontend-pedia/src/modules/index.js
+++ frontend-pedia/src/modules/index.js
@@ -3,23 +3,26 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import movie, { movieSaga } from './movie';
 import loading from './loading';
-import movies, { searchMoviesSaga, netChaRankingMoviesSaga, scoreMoviesSaga } from './movies';
-import actors, { actorsSaga } from './likeActors';
+import movies, { searchMoviesSaga, netchaRankingMoviesSaga, scoreMoviesSaga } from './movies';
+import actors, { actorsSaga } from './actors';
+import directors, { directorsSaga } from './directors';
 
 const rootReducer = combineReducers({
   loading,
   movie,
   movies,
   actors,
+  directors,
 });
 
 export function* rootSaga() {
   yield all([
     movieSaga(), 
     searchMoviesSaga(), 
-    netChaRankingMoviesSaga(),
+    netchaRankingMoviesSaga(),
     scoreMoviesSaga(),
-    actorsSaga()
+    actorsSaga(),
+    directorsSaga(),
   ]);
 }
 
