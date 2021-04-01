9a627b1643b397a38e9a7674c3ea83a1c769bbe2
leegw217 <leegw217@naver.com>
2021-04-01 23:23:21 +0900

    0401_BE : movie api update
    Former-commit-id: 555e9106afb064d79fa0de851e2dad7040fc7780

--- backend/netcha/src/main/java/com/netcha/movie/controller/MovieController.java
+++ backend/netcha/src/main/java/com/netcha/movie/controller/MovieController.java
@@ -95,8 +95,13 @@ public class MovieController {
 		return new ResponseEntity<>("success", HttpStatus.OK);
 	}
 	
-	
-	
+	@ApiOperation(value = "평가하기 페이지 (40개) : 평가한적 없는 영화, 누적 조회수 순으로 추천", notes = "입력값 : userId(유저고유번호), pageNum(페이지 번호(0번부터 시작))")
+	@GetMapping("/ranking_page")
+	public ResponseEntity<?> getListRankingPage(@RequestParam long userId, @RequestParam long pageNum) {
+		List<MovieResponseDto> movies = movieService.findMovieByNoNotInNo((int)userId, (int)pageNum);
+		System.out.println("평가하기 페이지 : "+movies.size());
+		return new ResponseEntity<>(movies, HttpStatus.OK);
+	}
 	
 	@GetMapping("/test")
 	public ResponseEntity<?> test() {
