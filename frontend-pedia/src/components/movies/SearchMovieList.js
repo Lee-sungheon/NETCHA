8dd42b7e9d7ae0cdc2075cd1a40117ae4607264b
강유정 <kos9811@naver.com>
2021-04-05 01:58:48 +0900

    0405_FE_pedia : 영화검색페이지 무한스크롤 기능 완료
    Former-commit-id: 3ba46118108401e95434deb59704de69316c7998

--- frontend-pedia/src/components/movies/SearchMovieList.js
+++ frontend-pedia/src/components/movies/SearchMovieList.js
@@ -1,17 +1,25 @@
-import React, { useCallback, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
-import "./SearchMovieList.scss";
+import React, { useCallback, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import './SearchMovieList.scss';
 
 const MovieItem = ({ movie }) => {
-  const { posterUrl, title, open, country, id } = movie;
+  const { posterUrl, title, open, country, no } = movie;
   return (
-    <Link to={`/movieDetail/${id}`}>
+    <Link to={`/movieDetail/${no}`}>
       <div className="movieWrap">
-        <img className="moviePoster" src={posterUrl === "default" ? "../../images/defaultPoster.png" : posterUrl} alt={title} />
+        <img
+          className="moviePoster"
+          src={
+            posterUrl === 'default'
+              ? '../../images/defaultPoster.png'
+              : posterUrl
+          }
+          alt={title}
+        />
         <div className="movieInfo">
           <div className="movieTitle">{title}</div>
           <div className="movieInfo">
-            {open.split('-')[0]} · {country}
+            {open? open.split('-')[0] : ''} · {country}
           </div>
         </div>
         <hr />
@@ -20,30 +28,13 @@ const MovieItem = ({ movie }) => {
   );
 };
 
-const SearchMovieList = ({ loading, movies, error, page, fetchMoreData }) => {
+const SearchMovieList = ({ movies }) => {
   const history = useHistory();
 
-  if (error) {
-    return <h2 style={{paddingTop: "100px"}}>에러가 발생했습니다.</h2>;
-  }
+  // if (error) {
+  //   return <h2 style={{ paddingTop: '100px' }}>에러가 발생했습니다.</h2>;
+  // }
 
-
-  const _infiniteScroll = useCallback(() => {
-    let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-    let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
-    let clientHeight = document.documentElement.clientHeight;
-
-    if(scrollTop + clientHeight === scrollHeight) {
-      // setItemIndex(itemIndex + 100);
-      // setResult(movies.concat(video_list.slice(itemIndex+100, itemIndex+200)));
-      fetchMoreData();
-    }
-  }, [page]);
-
-  useEffect(() => {
-    window.addEventListener('scroll', _infiniteScroll, true);
-    return () => window.removeEventListener('scroll', _infiniteScroll, true);
-  }, [_infiniteScroll]);
 
 
   return (
@@ -51,23 +42,31 @@ const SearchMovieList = ({ loading, movies, error, page, fetchMoreData }) => {
       <div className="searchMovieListWrap">
         <div className="movieHeaderWrap">
           <button className="beforeArrow" onClick={() => history.goBack()}>
-            <img className="beforeArrowImage" src="/images/beforeArrow.png" alt="이전페이지" />
+            <img
+              className="beforeArrowImage"
+              src="/images/beforeArrow.png"
+              alt="이전페이지"
+            />
           </button>
           <div className="movieHeader">영화</div>
         </div>
         <hr />
         {/* 로딩 중이 아니고, 영화 리스트가 존재할 때만 보여준다.*/}
-        {!loading && movies && (
+        {/* {!loading && movies && ( */}
+        {movies && (
           <div className="movieListWrap">
-            {movies.map((movie) => {
-              return <MovieItem movie={movie} key={movie.id} />;
+            {movies.map((movie, index) => {
+              return <MovieItem movie={movie} key={index} />;
             })}
+            {/* <div ref={setTarget} className="last-item">
+              <Loader size="s" />
+            </div> */}
           </div>
         )}
       </div>
-      <div>
+      {/* <div>
         <button onClick={fetchMoreData}>더보기</button>
-      </div>
+      </div> */}
     </>
   );
 };
---
+++ frontend-pedia/src/containers/movies/useInfiniteScroll.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+export const useInfinteScroll = ({
+  root = null,
+  target,
+  onIntersect,
+  threshold = 1.0,
+  rootMargin = '0px',
+}) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersect, {
+      root,
+      rootMargin,
+      threshold,
+    });
+    if (!target) {
+      return;
+    }
+    observer.observe(target);
+    return () => {
+      observer.unobserve(target);
+    };
+  }, [target, root, rootMargin, onIntersect, threshold]);
+};
