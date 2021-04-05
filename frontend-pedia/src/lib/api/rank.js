7e7bda4b183c4af1f439be3796b78920e621ba15
강민창 <new0822@naver.com>
2021-04-06 00:27:44 +0900

    0406_FE_pedia : 별점그래프 동기화
    Former-commit-id: 77ebece5ff84c1610450856d545f899dc1467d01

--- frontend-pedia/src/lib/api/rank.js
+++ frontend-pedia/src/lib/api/rank.js
@@ -3,3 +3,7 @@ import client from './client';
 export const updateRank = (formData) => {
   return client.post('/movie/rank_update', formData);
 };
+
+export const readRank = (movieNo) => {
+  return client.get('/movie/rank_movie', { params: movieNo });
+};
