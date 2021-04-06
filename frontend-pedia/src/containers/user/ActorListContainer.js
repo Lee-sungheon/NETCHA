90a1e74acf75b863be0eed5b0e08d9b5214c7e94
강유정 <kos9811@naver.com>
2021-04-06 15:17:23 +0900

    0406_FE_pedia : 오류수정
    Former-commit-id: ff24191600e1c07414debb884a42740434b0cd52

--- frontend-pedia/src/containers/user/ActorListContainer.js
+++ frontend-pedia/src/containers/user/ActorListContainer.js
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { withRouter } from "react-router";
 import { listActors } from "../../modules/actors";
 import Loader from '../../components/common/Loader';
-import crawlingTest from '../../crawlingTest';
 
 const actorArr = [];
 
