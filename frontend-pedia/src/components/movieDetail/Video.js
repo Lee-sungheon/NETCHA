44f4e68c692b958c7a854c3cb97d3f409a432b25
강민창 <new0822@naver.com>
2021-04-06 22:08:50 +0900

    0406_FE_pedia : 상세페이지 토큰 로그인
    Former-commit-id: 82c0c6e762c32ebd559093bceb0c1a1d4027c030

--- frontend-pedia/src/components/movieDetail/Video.js
+++ frontend-pedia/src/components/movieDetail/Video.js
@@ -1,17 +1,17 @@
-import axios from "axios";
-import { useState } from "react";
-import Slider from "react-slick";
-import YTSearch from "youtube-api-search";
-const API_KEY = "AIzaSyAF-_uQIzruH-iViU7EJXBvFhQrh39iDDU";
+import axios from 'axios';
+import { useState } from 'react';
+import Slider from 'react-slick';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyAF-_uQIzruH-iViU7EJXBvFhQrh39iDDU';
 
 const Video = () => {
   const [video, setVideo] = useState(null);
 
   const settings = {
-    className: "center",
+    className: 'center',
     centerMode: true,
     infinite: true,
-    centerPadding: "5px",
+    centerPadding: '5px',
     slidesToShow: 1,
     arrows: true,
     speed: 500,
@@ -19,17 +19,17 @@ const Video = () => {
     slidesPerRow: 2,
   };
 
-  const searchYouTube = ({ query, max, key }, callback) => {
-    axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${key}&q=${query}&maxResults=${max}&type=video&videoEmbeddable=true`
-      )
-      .then((response) => {
-        console.log(response);
-      });
-  };
+  // const searchYouTube = ({ query, max, key }, callback) => {
+  //   axios
+  //     .get(
+  //       `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${key}&q=${query}&maxResults=${max}&type=video&videoEmbeddable=true`
+  //     )
+  //     .then((response) => {
+  //       console.log(response);
+  //     });
+  // };
 
-  searchYouTube({ query: "아이언맨", max: 2, key: API_KEY });
+  // searchYouTube({ query: "아이언맨", max: 2, key: API_KEY });
   return (
     <div className="video">
       <div className="infoHeader">동영상</div>
