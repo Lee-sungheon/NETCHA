921626d46ac7ecc9df5ce6951a0ac808bf5fe15e
leegw217 <leegw217@naver.com>
2021-04-07 03:33:50 +0900

    0406_BE : DB update
    Former-commit-id: b192e951d824e75750e37020430bb9c100c1c607

--- backend/netcha/src/main/java/com/netcha/movie/service/MovieService.java
+++ backend/netcha/src/main/java/com/netcha/movie/service/MovieService.java
@@ -1262,7 +1262,8 @@ public class MovieService {
 	
 	public void test() {
 		List<Movie> list = movieRepository.findByOpenAndPosterUrl("2015-01-01", "");
-		for(Movie m : list) {
+		for(int i=0; i<100; i++) {
+			Movie m = list.get(i);
 			String[] result = crawling(m);
 			System.out.println("("+m.getTitle()+")"+result[0]+" "+result[1]+" "+result[2]);
 			m.updateCrawling(result[0], result[1], result[2]);
