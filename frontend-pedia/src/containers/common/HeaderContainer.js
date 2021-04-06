182cef30a491424d04487f8afbf7ca1b5245e22d
강유정 <kos9811@naver.com>
2021-04-07 02:51:52 +0900

    0407_FE_pedia : 로그인 오류 해결, 로그아웃 제거
    Former-commit-id: 2158cd738d8685f1704c77f00773dc61224141f1

--- frontend-pedia/src/containers/common/HeaderContainer.js
+++ frontend-pedia/src/containers/common/HeaderContainer.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/common/Header';
-import { setUser, logout } from '../../modules/user';
+import { setUser } from '../../modules/user';
 
 export default function HeaderContainer() {
   const { user } = useSelector(({ user }) => ({ user }.user));
   const dispatch = useDispatch();
-  const onLogout = () => {
-    console.log('로그아웃들어옴');
-    dispatch(logout());
-  };
   const onLogin = () => {
     console.log('로그인들어옴');
     const newUser = { seq: 1, nickname: '내가바로이름' };
@@ -17,5 +13,5 @@ export default function HeaderContainer() {
     dispatch(setUser(JSON.parse(localStorage.getItem('user'))));
   };
 
-  return <Header user={user} onLogout={onLogout} onLogin={onLogin} />;
+  return <Header user={user} onLogin={onLogin} />;
 }
