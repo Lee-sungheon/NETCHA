8d1f0438ec5fbb52c1a428dd1ebbbe3ed156efee
강유정 <kos9811@naver.com>
2021-04-07 02:21:01 +0900

    0407_FE_pedia : 영화상세페이지 동영상 연결 완료, 백 미완
    Former-commit-id: f5f5e71eb58030dfcfa0c95aae304c237c82a1f2

--- frontend-pedia/src/pages/MovieRankingPage.js
+++ frontend-pedia/src/pages/MovieRankingPage.js
@@ -1,20 +1,22 @@
-import React from "react";
-import Footer from "../components/common/Footer";
-import HeaderContainer from "../containers/common/HeaderContainer";
-import MovieRankingContainer from "../containers/movies/MovieRankingContainer";
-import NewMovieContainer from "../containers/movies/NewMovieContainer";
-import "./MovieRankingPage.scss";
+import React from 'react';
+import Footer from '../components/common/Footer';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import MovieRankingContainer from '../containers/movies/MovieRankingContainer';
+import NewMovieContainer from '../containers/movies/NewMovieContainer';
+import './MovieRankingPage.scss';
 
 export default function MovieRanking() {
   return (
     <>
       <HeaderContainer />
-      <div className="container1">
-        <div className="container2">
-          <NewMovieContainer title="최신 개봉 영화" />
-        </div>
-        <div className="container2">
-          <MovieRankingContainer title="넷챠 영화 순위" />
+      <div>
+        <div className="container1">
+          <div className="container2">
+            <NewMovieContainer title="최신 개봉 영화" />
+          </div>
+          <div className="container2">
+            <MovieRankingContainer title="넷챠 영화 순위" />
+          </div>
         </div>
       </div>
       <Footer />
