a6a832cc9f3ffaefdf268f2643e21c5e8f436983
강민창 <new0822@naver.com>
2021-04-06 18:59:46 +0900

    0406_FE_pedia : 정리
    Former-commit-id: f17b9d6f4f35088082484e07ad3d9732081a9e6b

--- frontend-pedia/src/containers/movieDetail/MovieDetailContainer.js
+++ frontend-pedia/src/containers/movieDetail/MovieDetailContainer.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { readMovie, unloadMovie } from '../../modules/movie';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { readMovie, unloadMovie } from "../../modules/movie";
 
-import './MovieDetailContainer.scss';
-import MovieHeader from '../../components/movieDetail/MovieHeader';
-import BasicInfo from '../../components/movieDetail/BasicInfo';
-import Cast from '../../components/movieDetail/Cast';
-import StarGraph from '../../components/movieDetail/StarGraph';
-import Comment from '../../components/movieDetail/comment/Comment';
-import SimilarMovies from '../../components/movieDetail/SimilarMovies';
-import Gallery from '../../components/movieDetail/Gallery';
-import Video from '../../components/movieDetail/Video';
-import WriteComment from '../../components/movieDetail/comment/WriteComment';
-import MyComment from '../../components/movieDetail/comment/MyComment';
+import "./MovieDetailContainer.scss";
+import MovieHeader from "../../components/movieDetail/MovieHeader";
+import BasicInfo from "../../components/movieDetail/BasicInfo";
+import Cast from "../../components/movieDetail/Cast";
+import StarGraph from "../../components/movieDetail/StarGraph";
+import Comment from "../../components/movieDetail/comment/Comment";
+import SimilarMovies from "../../components/movieDetail/SimilarMovies";
+import Gallery from "../../components/movieDetail/Gallery";
+import Video from "../../components/movieDetail/Video";
+import WriteComment from "../../components/movieDetail/comment/WriteComment";
+import MyComment from "../../components/movieDetail/comment/MyComment";
 
 const MovieDetailContainer = ({ match }) => {
   // 처음 마운트될 때 무비 읽기 API 요청
-  const { movieNo } = match.params;
+  const { movieNo, token } = match.params;
   const dispatch = useDispatch();
   const [rankData, setRankData] = useState({ ranking: 0 });
   const [zzimData, setZzimData] = useState({ isZzim: false });
-  const [myCommentData, setMyCommentData] = useState({ content: '' });
+  const [myCommentData, setMyCommentData] = useState({ content: "" });
 
   const { movie, user, requestData, error, loading } = useSelector(
     ({ movie, user, loading }) => ({
@@ -32,7 +32,7 @@ const MovieDetailContainer = ({ match }) => {
         movieNo: parseInt(match.params.movieNo),
         userId: user.user.userId,
       },
-      loading: loading['movie/READ_MOVIE'],
+      loading: loading["movie/READ_MOVIE"],
     })
   );
 
