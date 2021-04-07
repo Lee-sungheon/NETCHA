da9637f94804de8dd978e0b19eaf43d1a9fea3b9
강유정 <kos9811@naver.com>
2021-04-08 03:59:03 +0900

    0408_FE_pedia : 영화 검색 완료
    Former-commit-id: 04de799362433d0db4c201bee98f8b4f637c2707

--- frontend-pedia/src/containers/movies/SearchMovieListContainer.js
+++ frontend-pedia/src/containers/movies/SearchMovieListContainer.js
@@ -1,87 +1,71 @@
-import React, { useEffect, useState, useCallback } from "react";
-import qs from "qs";
-import MovieList from "../../components/movies/MovieList";
-import { withRouter } from "react-router";
-import * as moviesApi from "../../lib/api/movies";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState, useCallback } from 'react';
+import qs from 'qs';
+import MovieList from '../../components/movies/MovieList';
+import { withRouter } from 'react-router';
+import * as moviesApi from '../../lib/api/movies';
+import { useSelector } from 'react-redux';
 
-const SearchMovieListContainer = ({ location }) => {
+// let page = 0;
+const SearchMovieListContainer = ({ location, keyword }) => {
+  const [stop, setStop] = useState(false);
   const [loading, setLoading] = useState(null);
-  const [reqStop, setReqStop] = useState(false);
   const [page, setPage] = useState(0);
   const [movies, setMovies] = useState([]);
-  const { userId, keyword } = useSelector(({ user, autoCompletesMovies }) => ({
-    keyword: autoCompletesMovies.keyword.keyword,
-    userId: user.user.userId,
+  const { userId } = useSelector(({ user }) => ({
+    userId: user.user.userId
   }));
-  // const { keyword } = qs.parse(location.search, {
-  //   ignoreQueryPrefix: true,
-  // });
+
+  const initSearchMovies = async () => {
+    setLoading(true);
+    const response = await moviesApi.listSearchMovies({ page, keyword, userId });
+    
+    setMovies(response.data);
+    
+    setLoading(false);
+  };
 
   const getSearchMovies = async (newPage) => {
     setLoading(true);
-
-    await moviesApi
-      .listSearchMovies({ keyword, page: newPage, userId })
-      .then((response) => {
-        const fetchedData = response.data;
-
-        const mergedData = movies.concat(...fetchedData);
-        setMovies(mergedData);
-      });
-
-    // const response = await moviesApi.listSearchMovies({ keyword, page: newPage, userId });
-    // // if (movies) {
-    //   console.dir(movies);
-    //   console.dir(response.data);
-    //   // setMovies([...movies, ...response.data]);
-    //   setMovies(movies.concat(...response.data.data));
-    // }
-    // else {
-    //   setMovies(response.data);
-    // }
+    const response = await moviesApi.listSearchMovies({ page: newPage, keyword, userId });
+    
+    if(response.data.length === 0) {
+      setStop(true);
+      setLoading(false);
+      return;
+    }
+      setMovies(movies.concat(...response.data));
+      
     setLoading(false);
   };
 
   useEffect(() => {
-    getSearchMovies(page + 1);
+    initSearchMovies();
   }, []);
 
   const _infiniteScroll = useCallback(() => {
-    console.log("scroll");
-    let scrollHeight = Math.max(
-      document.documentElement.scrollHeight,
-      document.body.scrollHeight
-    );
-    let scrollTop = Math.max(
-      document.documentElement.scrollTop,
-      document.body.scrollTop
-    );
+    if(stop) return;
+    console.log('scroll');
+    let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+    let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
     let clientHeight = document.documentElement.clientHeight;
-
-    if (scrollTop + clientHeight === scrollHeight) {
-      console.log("fetchMore");
-      setPage(page + 1);
+    
+    if(scrollTop + clientHeight === scrollHeight) {
+      console.log('fetchMore');
+      setPage(page+1);
       getSearchMovies(page+1);
-      
-        // setMovies(movies.concat(getSearchMovies(page+1)));
     }
-  }, [page]);
+  }, [page, movies]);
 
   useEffect(() => {
-    window.addEventListener("scroll", _infiniteScroll, true);
-    return () => window.removeEventListener("scroll", _infiniteScroll, true);
+    window.addEventListener('scroll', _infiniteScroll, true);
+    return () => window.removeEventListener('scroll', _infiniteScroll, true);
   }, [_infiniteScroll]);
 
   return (
     <>
       <MovieList movies={movies} headerTitle="영화 검색 결과" />
-      {loading && (
-        <img src="/images/Rolling-50px.svg" style={{ marginLeft: "50%" }} />
-      )}
-      <button style={{ color: "white", backgroundColor: "white" }}>
-        무한스크롤
-      </button>
+      {loading && <img src="/images/Rolling-50px.svg" style={{marginLeft: "50%"}}/>}
+      <button style={{color: "white", backgroundColor: "white"}}>무한스크롤</button>
     </>
   );
 };
