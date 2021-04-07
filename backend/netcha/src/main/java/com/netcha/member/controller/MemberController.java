976099b26ff2daa76b8105f13cdd1239578b87d0
강민창 <new0822@naver.com>
2021-04-08 02:42:46 +0900

    0408_BE : user2
    Former-commit-id: 84108a49bade541be7d5c0b9a2e7e8e940d7b163

--- backend/netcha/src/main/java/com/netcha/member/controller/MemberController.java
+++ backend/netcha/src/main/java/com/netcha/member/controller/MemberController.java
@@ -94,12 +94,20 @@ public class MemberController {
 	public Response getToken(@PathVariable String token, HttpServletRequest req, HttpServletResponse res) {
 		try {
 			Member member = authService.findByUserId(jwtUtil.getUsername(token));
+
 			Cookie accessToken = cookieUtil.createCookie(JwtUtil.ACCESS_TOKEN_NAME, token);
-			Cookie refreshToken = cookieUtil.createCookie(JwtUtil.REFRESH_TOKEN_NAME, redisUtil.getData(member.getUserId()));
-			res.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
-			res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-			res.addCookie(accessToken);
-			res.addCookie(refreshToken);
+			Cookie refreshToken = cookieUtil.createCookie(JwtUtil.REFRESH_TOKEN_NAME,
+					redisUtil.getData(member.getUserId()));
+			ResponseCookie at = ResponseCookie.from("accessToken", accessToken.getValue()).sameSite("None")
+					.maxAge(accessToken.getMaxAge()).httpOnly(true).secure(true).build();
+			ResponseCookie rt = ResponseCookie.from("refreshToken", refreshToken.getValue()).sameSite("None")
+					.maxAge(refreshToken.getMaxAge()).httpOnly(true).secure(true).build();
+
+			res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+			res.setHeader("Access-Control-Allow-Headers",
+					"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+			res.addHeader("Set-Cookie", at.toString());
+			res.addHeader("Set-Cookie", rt.toString());
 			MemberResponseDto userInfo = new MemberResponseDto(member);
 			return new Response("success", "유저정보 조회 성공", userInfo);
 		} catch (Exception e) {
