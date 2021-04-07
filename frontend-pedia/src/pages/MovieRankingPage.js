b3e8b3c79a343a7e4a5953bfdd437b02015da717
강민창 <new0822@naver.com>
2021-04-08 04:36:43 +0900

    0408_FE_pedia : 코드 복구
    Former-commit-id: d732fab8be78c28d6864ce8b02389cd09ad50e18

--- frontend-pedia/src/pages/MovieRankingPage.js
+++ frontend-pedia/src/pages/MovieRankingPage.js
@@ -8,20 +8,12 @@ import * as authApi from '../lib/api/auth';
 import { setUser } from '../modules/user';
 import { useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { useCookies } from 'react-cookie';
 
 const MovieRanking = ({ match, history }) => {
-  const [cookies, setCookie] = useCookies(['name']);
-
   const { token } = match.params;
   const dispatch = useDispatch();
   const getUserInfo = async () => {
     const response = await authApi.getUserInfo(token);
-    setCookie(
-      'accessToken',
-      'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTYxNzgxOTQ5NywiZXhwIjoxNjE3ODMzODk3fQ.Kf4abMdMnRKdiGsnw_v3YLCgyVakM2WNiU46fSP_WYs',
-      { path: '/' }
-    );
 
     dispatch(setUser(response.data.data));
     localStorage.setItem('user', JSON.stringify(response.data.data));
