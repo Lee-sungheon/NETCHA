29d1021aea4d5d155bb9c3667ee0e9aff80794c6
강민창 <new0822@naver.com>
2021-04-09 10:14:46 +0900

    0409_BE : 쿠키 만료시간 변경
    Former-commit-id: 386d9168d8c8fcc4a01c719a294ca30f5744a0f3

--- exec/backend/netcha/src/main/java/com/netcha/member/service/JwtUtil.java
+++ exec/backend/netcha/src/main/java/com/netcha/member/service/JwtUtil.java
@@ -19,8 +19,8 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtUtil {
 
-    public final static long TOKEN_VALIDATION_SECOND = 1000L * 60 * 24 * 10; // 10일
-    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 24 * 30; // 30일
+    public final static long TOKEN_VALIDATION_SECOND = 1000L * 5; // 5분
+    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 24 * 1; // 1일
 
     final static public String ACCESS_TOKEN_NAME = "accessToken";
     final static public String REFRESH_TOKEN_NAME = "refreshToken";
